1ec8a6cb88fe677a287aeb46b4a2dc407b330d7a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-01-14 11:18:42 -0800

    Debugged the PlantList

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,23 +1,27 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
-    plants:[]
+    plants: [],
   }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
   componentDidMount() {
-    axios.get('http://localhost:3333/plants')
-    .then((resp) => {
-      console.log(resp)
-      this.setState({
-        ...this.state,
-        plants:resp.data,
+    axios
+      .get('http://localhost:3333/plants')
+      .then((resp) => {
+        console.log(resp)
+        this.setState({
+          ...this.state,
+          plants: resp.data,
+        })
       })
-    })
+      .catch((err) => {
+        console.log(err.data)
+      })
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
@@ -26,7 +30,12 @@ export default class PlantList extends Component {
       <main className="plant-list">
         {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id} data-testid="plant-card">
-            <img className="plant-image" width="280" src={plant.img} alt={plant.name} />
+            <img
+              className="plant-image"
+              width="280"
+              src={plant.img}
+              alt={plant.name}
+            />
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
               <p className="plant-scientific-name">{plant.scientificName}</p>
@@ -46,6 +55,6 @@ export default class PlantList extends Component {
           </div>
         ))}
       </main>
-    );
+    )
   }
 }
